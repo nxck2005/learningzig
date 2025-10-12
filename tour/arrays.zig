8b24fe0a56c1588517ef1ec9d427f456ef4f2678
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-13 03:42:20 +0530

    Add len infer

--- tour/arrays.zig
+++ tour/arrays.zig
@@ -5,6 +5,7 @@ pub fn main() !void {
     // (i.e., the array's child type).
     // For array literals, N may be replaced by _ to infer the size of the array.
     // For length, access arr.len field.
+    // replace N with _ to infer size
     const arr = [5]u8{ 1, 2, 3, 4, 5 };
     const len = arr.len;
     std.debug.print("{d}\n", .{len});
