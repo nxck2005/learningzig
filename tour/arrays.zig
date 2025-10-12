4b4c487076a9f44538f9122e5e9983d7be881a9d
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-13 01:25:18 +0530

    Add arrays

---
+++ tour/arrays.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // Arrays are denoted by [N]T, where N is the number of elements in the array and T is the type of those elements,
+    // (i.e., the array's child type).
+    // For array literals, N may be replaced by _ to infer the size of the array.
+    // For length, access arr.len field.
+    const arr = [5]u8{ 1, 2, 3, 4, 5 };
+    const len = arr.len;
+    std.debug.print("{d}\n", .{len});
+}
