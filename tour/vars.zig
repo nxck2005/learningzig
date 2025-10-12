6b380bab15f16daeca42e08d16f161227bd8387e
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-13 01:20:40 +0530

    Add type coercion and undefined kw

--- tour/vars.zig
+++ tour/vars.zig
@@ -7,6 +7,15 @@ pub fn main() !void {
     var maxu8: u8 = 255;
     maxu8 -= 1;
 
+    // inferred type coercion
+    var k = @as(i32, 5000);
+    k = k + 1;
+
+    // when val is not known
+
+    var z: u8 = undefined;
+    z = z + 1;
+
     const MAX: u8 = 255;
     std.debug.print("{d}\n", .{MAX});
 }
